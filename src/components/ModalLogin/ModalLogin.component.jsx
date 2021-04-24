0b593ce3d1be47ed2e3e208380d6534a92e7a4bb
Jose Luis Rodriguez <josrod@Joses-Mac-mini.local>
2021-04-24 16:51:33 -0500

    Now capable of logging out

--- src/components/ModalLogin/ModalLogin.component.jsx
+++ src/components/ModalLogin/ModalLogin.component.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useMemo, useState } from 'react';
 import ReactDOM from 'react-dom';
 import loginApi from '../../login.api';
 import { useAuth } from '../../providers/Auth/Auth.provider';
@@ -38,19 +38,32 @@ const ModalLogin = ({ theme }) =>  {
 
     }
 
+    function handleLogoutButtonClick() {
+        logout();
+        showOrHideModalLogin();
+    }
+
     return ReactDOM.createPortal(<Modal>
         <ModalStructure theme={theme}>
             <div style={{textAlign: 'right'}}>
-                <ButtonHoverItem style={{ color: "red" }} onClick={showOrHideModalLogin}>X</ButtonHoverItem>
+                <ButtonHoverItem style={{ fontWeight: "800" }} onClick={showOrHideModalLogin}>X</ButtonHoverItem>
             </div>
-            <h2 style={{ margin: "0px", }}>Login</h2>
-            <div style={{ paddingLeft:"50px", paddingRight:"50px" }}>
+            <h2 style={{ margin: "0px", }}>{ authenticated ? "Log out" : "Login"}</h2>
+            {/* { authenticated ? <LoginContent/> : <LogoutContent /> } */}
+            { !authenticated ? 
+                <div style={{ paddingLeft:"50px", paddingRight:"50px" }}>
                 <NiceInput placeholder="email" withLabel="email" value={email} setValue={setEmail} ></NiceInput>
                 <NiceInput placeholder="password" withLabel="password" 
                     isPassword={true} value={password} setValue={setPassword} ></NiceInput>
                 { errorLogin ? <p style={{ color: "red", fontSize: "small", marginTop: "10px", fontWeight: "700" }}>Invalid credentials</p> : <></> }
                 <ButtonHome style={{ marginTop: !errorLogin ? "20px" : "0px", }} theme={theme} onClick={handleLoginButtonClick}>Sign in</ButtonHome>
             </div>
+            :
+            <div style={{ paddingLeft:"50px", paddingRight:"50px", display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <p>You are logged in.</p>
+                <ButtonHome style={{ marginTop: "20px", }} theme={theme} onClick={handleLogoutButtonClick}>Sign out</ButtonHome>
+            </div> 
+            }
         </ModalStructure>
     </Modal>, document.getElementById('modallogin')); 
 }
